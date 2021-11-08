1c4259692c8a1793380446bb1798b91c79c7a20b
00014973 <fozilzayniddinov7777@gmail.com>
2021-11-08 08:26:57 +0500

    filter logic

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,17 @@
+// navigation vars
+
 let navMenu = document.getElementById("nav-menu");
 let navBarToggle = document.getElementById("nav-burger");
 let navBurger = document.getElementById("nav-toggle-btn");
-
 let anchorNav = document.querySelectorAll('#anchor');
 
+
+// responsive filter vars
+let filterConfig = document.querySelector(".filter-config")
+let filterClose = document.getElementById("close")
+let filterBody = document.querySelector(".filter")
+
+// navigation drop-down burger logic
 navBarToggle.addEventListener("click", function () {
     navMenu.classList.toggle('menu-active')
     navBurger.classList.toggle("active");
@@ -15,4 +23,14 @@ anchorNav.forEach(link => {
         navMenu.classList.toggle('menu-active')
         navBurger.classList.toggle("active");
     })
+})
+
+
+// filter config body toggle
+filterConfig.addEventListener('click', () => {
+    filterBody.classList.add("active")
+})
+
+filterClose.addEventListener('click', () => {
+    filterBody.classList.remove("active")
 })
